6027dabb66d6502c76a92c264c1657d3a336ad58
s44sfh9hr7-wq <s44sfh9hr7@privaterelay.appleid.com>
2026-09-21 14:33:44 +0200

    Add files via upload

---
+++ assets/service-worker.js
@@ -0,0 +1,14 @@
+
+const CACHE = "bkl-prototype-v080";
+const ASSETS = [
+  "./","./index.html","./styles.css","./app.js",
+  "./assets/bkl-banner.jpeg","./assets/bkl-logo.png",
+  "./assets/apple-touch-icon.png","./assets/icon-192.png","./assets/icon-512.png"
+  ,"./bkl-hymne.mp3"
+];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
